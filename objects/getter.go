466fb31034195c2ea40904fdf33ffc3ef5ac20ec
Christian Ezeani <christian.ezeani@gmail.com>
2022-05-01 23:54:56 +0100

    Add getter to `objects` package

---
+++ objects/getter.go
@@ -0,0 +1,28 @@
+package objects
+
+import (
+	"reflect"
+
+	"cyberpull.com/go-cyb/errors"
+)
+
+type Getter interface {
+	Get(key string) (value any, exists bool)
+}
+
+func Get[T any](g Getter, key string) (value T, err error) {
+	var ok bool
+	var tmpValue any
+
+	if tmpValue, ok = g.Get(key); !ok {
+		err = errors.Newf(`Key "%s" does not exist`, key)
+		return
+	}
+
+	if value, ok = tmpValue.(T); !ok {
+		rType := reflect.TypeOf(value)
+		err = errors.Newf(`Value is not of type "%s"`, rType.Elem().String())
+	}
+
+	return
+}
