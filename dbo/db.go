da26b5928f4dbe2079fc2836088ef3fc391c1e61
Christian Ezeani <christian.ezeani@gmail.com>
2022-05-08 19:16:56 +0100

    Move `DB()` function from `database` to `dbo`

---
+++ dbo/db.go
@@ -0,0 +1,24 @@
+package dbo
+
+import "cyberpull.com/go-cyb/errors"
+
+type DBFunction func(tx ...*TxDB) (value *TxDB, err error)
+
+func DB(db *TxDB) DBFunction {
+	// Database Function
+	return func(tx ...*TxDB) (value *TxDB, err error) {
+		if len(tx) > 0 && tx[0] != nil {
+			value = tx[0]
+			return
+		}
+
+		if db != nil {
+			value = db
+			return
+		}
+
+		err = errors.New("Database connection not found")
+
+		return
+	}
+}
