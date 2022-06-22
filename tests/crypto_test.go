920fd5a3c5db6c5ab1d9a748e9bee4944bf4c266
Christian Ezeani <christian.ezeani@gmail.com>
2022-06-22 11:45:55 +0100

    Add test cases for `crypto` package

--- tests/crypto_test.go
+++ tests/crypto_test.go
@@ -4,8 +4,38 @@ import (
 	"testing"
 
 	_ "cyberpull.com/go-cyb/env"
+
+	"cyberpull.com/go-cyb/crypto"
 )
 
-func TestCrypto(t *testing.T) {
-	//
+const CryptoKey string = "804w8irdjfry9573l2348jd0"
+
+var (
+	cryptoPlainText string = "Testing Data"
+	cryptoEncrypted string
+)
+
+func TestCrypto_Encrypt(t *testing.T) {
+	var err error
+
+	cryptoEncrypted, err = crypto.EncryptAES(cryptoPlainText, CryptoKey)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCrypto_Decrypt(t *testing.T) {
+	var err error
+
+	value, err := crypto.DecryptAES(cryptoEncrypted, CryptoKey)
+
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	if value != cryptoPlainText {
+		t.Fatalf(`Expected "%s", got "%s" instead`, cryptoPlainText, value)
+	}
 }
